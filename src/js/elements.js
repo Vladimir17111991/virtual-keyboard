b563b51b8796a3660173179bb3bcec487c9ef915
Vladimir <vovchikzaharov1991@gmail.com>
2022-05-06 17:54:23 +0300

    feat: added keyboards on the elements.js

---
+++ src/js/elements.js
@@ -0,0 +1,390 @@
+const elements = [
+    /* =========================================================================== line 1 ==================================================================================*/
+    [
+        {
+            key: { ru: 'ё', en: '`' },
+            pressShift: { ru: 'Ё', en: '~' },
+            shiftKey: true,
+            code: 'Backquote'
+        },
+        {
+            key: { ru: '1', en: '1' },
+            pressShift: { ru: '!', en: '!' },
+            shiftKey: true,
+            code: 'Digit1'
+        },
+        {
+            key: { ru: '2', en: '2' },
+            pressShift: { ru: '"', en: '@' },
+            shiftKey: true,
+            code: 'Digit2'
+        },
+        {
+            key: { ru: '3', en: '3' },
+            pressShift: { ru: '№', en: '#' },
+            shiftKey: true,
+            code: 'Digit3'
+        },
+        {
+            key: { ru: '4', en: '4' },
+            pressShift: { ru: ';', en: '$' },
+            shiftKey: true,
+            code: 'Digit4'
+        },
+        {
+            key: { ru: '5', en: '5' },
+            pressShift: { ru: '%', en: '%' },
+            shiftKey: true,
+            code: 'Digit5'
+        },
+        {
+            key: { ru: '6', en: '6' },
+            pressShift: { ru: ':', en: '^' },
+            shiftKey: true,
+            code: 'Digit6'
+        },
+        {
+            key: { ru: '7', en: '7' },
+            pressShift: { ru: '?', en: '&' },
+            shiftKey: true,
+            code: 'Digit7'
+        },
+        {
+            key: { ru: '8', en: '8' },
+            pressShift: { ru: '*', en: '*' },
+            shiftKey: true,
+            code: 'Digit8'
+        },
+        {
+            key: { ru: '9', en: '9' },
+            pressShift: { ru: '(', en: '(' },
+            shiftKey: true,
+            code: 'Digit9'
+        },
+        {
+            key: { ru: '0', en: '0' },
+            pressShift: { ru: ')', en: ')' },
+            shiftKey: true,
+            code: 'Digit0'
+        },
+        {
+            key: { ru: '-', en: '-' },
+            pressShift: { ru: '_', en: '_' },
+            shiftKey: true,
+            code: 'Minus'
+        },
+        {
+            key: { ru: '=', en: '=' },
+            pressShift: { ru: '+', en: '+' },
+            shiftKey: true,
+            code: 'Equal'
+        },
+        {
+            key: '←',
+            shiftKey: false,
+            class: 'backspace',
+            code: 'Backspace'
+        },
+    ],
+    /* =========================================================================== line 2 ==================================================================================*/
+    [
+        {
+            key: 'Tab',
+            code: 'Tab',
+            class: 'tab',
+            shiftKey: false,
+        },
+        {
+            key: { ru: 'й', en: 'q' },
+            pressShift: { ru: 'Й', en: 'Q' },
+            shiftKey: true,
+            code: 'KeyQ'
+        },
+        {
+            key: { ru: 'ц', en: 'w' },
+            pressShift: { ru: 'Ц', en: 'W' },
+            shiftKey: true,
+            code: 'KeyW'
+        },
+        {
+            key: { ru: 'у', en: 'e' },
+            pressShift: { ru: 'У', en: 'E' },
+            shiftKey: true,
+            code: 'KeyE'
+        },
+        {
+            key: { ru: 'к', en: 'r' },
+            pressShift: { ru: 'К', en: 'R' },
+            shiftKey: true,
+            code: 'KeyR'
+        },
+        {
+            key: { ru: 'е', en: 't' },
+            pressShift: { ru: 'Е', en: 'T' },
+            shiftKey: true,
+            code: 'KeyT'
+        },
+        {
+            key: { ru: 'н', en: 'y' },
+            pressShift: { ru: 'Н', en: 'Y' },
+            shiftKey: true,
+            code: 'KeyY'
+        },
+        {
+            key: { ru: 'г', en: 'u' },
+            pressShift: { ru: 'Г', en: 'U' },
+            shiftKey: true,
+            code: 'KeyU'
+        },
+        {
+            key: { ru: 'ш', en: 'i' },
+            pressShift: { ru: 'Ш', en: 'I' },
+            shiftKey: true,
+            code: 'KeyI'
+        },
+        {
+            key: { ru: 'щ', en: 'o' },
+            pressShift: { ru: 'Щ', en: 'O' },
+            shiftKey: true,
+            code: 'KeyO'
+        },
+        {
+            key: { ru: 'з', en: 'p' },
+            pressShift: { ru: 'З', en: 'P' },
+            shiftKey: true,
+            code: 'KeyP'
+        },
+        {
+            key: { ru: 'х', en: '[' },
+            pressShift: { ru: 'Х', en: '{' },
+            shiftKey: true,
+            code: 'BracketLeft'
+        },
+        {
+            key: { ru: 'ъ', en: ']' },
+            pressShift: { ru: 'Ъ', en: '}' },
+            shiftKey: true,
+            code: 'BracketRight'
+        },
+
+    ],
+    /* =========================================================================== line 3 ==================================================================================*/
+    [
+        {
+            key: 'CapsLock',
+            code: 'CapsLock',
+            class: 'capslock',
+            shiftKey: false,
+        },
+        {
+            key: { ru: 'ф', en: 'a' },
+            pressShift: { ru: 'Ф', en: 'A' },
+            shiftKey: true,
+            code: 'KeyA'
+        },
+        {
+            key: { ru: 'ы', en: 's' },
+            pressShift: { ru: 'Ы', en: 'S' },
+            shiftKey: true,
+            code: 'KeyS'
+        },
+        {
+            key: { ru: 'в', en: 'd' },
+            pressShift: { ru: 'В', en: 'D' },
+            shiftKey: true,
+            code: 'KeyD'
+        },
+        {
+            key: { ru: 'а', en: 'f' },
+            pressShift: { ru: 'А', en: 'F' },
+            shiftKey: true,
+            code: 'KeyF'
+        },
+        {
+            key: { ru: 'п', en: 'g' },
+            pressShift: { ru: 'П', en: 'G' },
+            shiftKey: true,
+            code: 'KeyG'
+        },
+        {
+            key: { ru: 'р', en: 'h' },
+            pressShift: { ru: 'Р', en: 'H' },
+            shiftKey: true,
+            code: 'KeyH'
+        },
+        {
+            key: { ru: 'о', en: 'j' },
+            pressShift: { ru: 'О', en: 'J' },
+            shiftKey: true,
+            code: 'KeyJ'
+        },
+        {
+            key: { ru: 'л', en: 'k' },
+            pressShift: { ru: 'Л', en: 'K' },
+            shiftKey: true,
+            code: 'KeyK'
+        },
+        {
+            key: { ru: 'д', en: 'l' },
+            pressShift: { ru: 'Д', en: 'L' },
+            shiftKey: true,
+            code: 'KeyL'
+        },
+        {
+            key: { ru: 'ж', en: ';' },
+            pressShift: { ru: 'Ж', en: ':' },
+            shiftKey: true,
+            code: 'Semicolon'
+        },
+        {
+            key: { ru: 'э', en: '\'' },
+            pressShift: { ru: 'Э', en: '"' },
+            shiftKey: true,
+            code: 'Quote'
+        },
+        {
+            key: { ru: '\\', en: '\\' },
+            pressShift: { ru: '|', en: '|' },
+            shiftKey: true,
+            code: 'Backslash',
+            class: 'backslash',
+        },
+        {
+            key: 'Enter',
+            code: 'Enter',
+            class: 'enter',
+            shiftKey: false,
+        },
+    ],
+
+    /* =========================================================================== line 4 ==================================================================================*/
+    [
+        {
+            key: 'Shift',
+            code: 'ShiftLeft',
+            class: 'leftshift',
+            shiftKey: false,
+        },
+        {
+            key: { ru: 'я', en: 'z' },
+            pressShift: { ru: 'Я', en: 'Z' },
+            shiftKey: true,
+            code: 'KeyZ'
+        },
+        {
+            key: { ru: 'ч', en: 'x' },
+            pressShift: { ru: 'Ч', en: 'X' },
+            shiftKey: true,
+            code: 'KeyX'
+        },
+        {
+            key: { ru: 'с', en: 'c' },
+            pressShift: { ru: 'С', en: 'C' },
+            shiftKey: true,
+            code: 'KeyC'
+        },
+        {
+            key: { ru: 'м', en: 'v' },
+            pressShift: { ru: 'М', en: 'V' },
+            shiftKey: true,
+            code: 'KeyV'
+        },
+        {
+            key: { ru: 'и', en: 'b' },
+            pressShift: { ru: 'И', en: 'B' },
+            shiftKey: true,
+            code: 'KeyB'
+        },
+        {
+            key: { ru: 'т', en: 'n' },
+            pressShift: { ru: 'Т', en: 'N' },
+            shiftKey: true,
+            code: 'KeyN'
+        },
+        {
+            key: { ru: 'ь', en: 'm' },
+            pressShift: { ru: 'Ь', en: 'M' },
+            shiftKey: true,
+            code: 'KeyM'
+        },
+        {
+            key: { ru: 'б', en: ',' },
+            pressShift: { ru: 'Б', en: '<' },
+            shiftKey: true,
+            code: 'Comma'
+        },
+        {
+            key: { ru: 'ю', en: '.' },
+            pressShift: { ru: 'Ю', en: '>' },
+            shiftKey: true,
+            code: 'Period'
+        },
+        {
+            key: { ru: '.', en: '/' },
+            pressShift: { ru: ',', en: '?' },
+            shiftKey: true,
+            code: 'Slash'
+        },
+
+        {
+            key: 'Shift',
+            code: 'ShiftRight',
+            class: 'rightshift',
+            shiftKey: false,
+        },
+        {
+            key: '↑',
+            code: 'ArrowUp',
+            shiftKey: false,
+        },
+    ],
+    [
+        {
+            key: 'Ctrl',
+            code: 'ControlLeft',
+            class: 'leftctrl',
+            shiftKey: false,
+        },
+        {
+            key: 'Alt',
+            code: 'AltLeft',
+            class: 'leftalt',
+            shiftKey: false,
+        },
+        {
+            key: ' ',
+            code: 'Space',
+            shiftKey: false,
+            class: 'space'
+        },
+        {
+            key: 'Alt',
+            code: 'AltRight',
+            class: 'rightalt',
+            shiftKey: false,
+        },
+        {
+            key: 'Ctrl',
+            code: 'ControlRight',
+            class: 'rightctrl',
+            shiftKey: false,
+        },
+        {
+            key: '←',
+            code: 'ArrowLeft',
+            shiftKey: false,
+        },
+        {
+            key: '↓',
+            code: 'ArrowDown',
+            shiftKey: false,
+        },
+        {
+            key: '→',
+            code: 'ArrowRight',
+            shiftKey: false,
+        },
+    ],
+];
+
+export default elements;
